5c329e594f3e28ffd04f0a60f34e1b49a7556db6
Matthew Slauson <matthew@slauson.io>
2024-01-07 11:26:23 -0600

    add docs

--- general.go
+++ general.go
@@ -8,6 +8,11 @@ import (
 	"github.com/slausonio/go-webserver/environment"
 )
 
+// SetEnvVarForTest sets an environment variable for testing purposes.
+// It takes a testing.T object, a key string, and a value string as parameters.
+// It uses the os.Setenv function to set the environment variable with the provided key and value.
+// If there is an error during the setting of the environment variable, it will call the t.Fatal function, passing the error.
+// The t.Helper function is called to mark this function as a test helper function and will be skipped during test coverage analysis.
 func SetEnvVarForTest(t *testing.T, key, value string) {
 	t.Helper()
 
@@ -17,12 +22,16 @@ func SetEnvVarForTest(t *testing.T, key, value string) {
 	}
 }
 
+// SetCurrentEnvForTest sets the current environment to "test" for testing purposes.
+// It takes a *testing.T parameter and calls the SetEnvVarForTest function to set the environment variable "environment.CurrentEnv" to "test".
+// If an error occurs during setting the environment variable, it will fail the test.
 func SetCurrentEnvForTest(t *testing.T) {
 	t.Helper()
 
 	SetEnvVarForTest(t, environment.CurrentEnv, "test")
 }
 
+// WriteEnvToFile writes the environment variables to a file in the specified directory.
 func WriteEnvToFile(t *testing.T, env environment.Environment) {
 	t.Helper()
 
