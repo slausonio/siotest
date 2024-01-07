ede4f38c18da24b2342c1286772c83d601a34151
Matthew Slauson <matthew@slauson.io>
2024-01-07 11:24:23 -0600

    add

---
+++ general.go
@@ -0,0 +1,34 @@
+package siotest
+
+import (
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/slausonio/go-webserver/environment"
+)
+
+func SetEnvVarForTest(t *testing.T, key, value string) {
+	t.Helper()
+
+	err := os.Setenv(key, value)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
+
+func SetCurrentEnvForTest(t *testing.T) {
+	t.Helper()
+
+	SetEnvVarForTest(t, environment.CurrentEnv, "test")
+}
+
+func WriteEnvToFile(t *testing.T, env environment.Environment) {
+	t.Helper()
+
+	err := godotenv.Write(env, "env/.env")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/slausonio/siotest
 
 go 1.21.5
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/slausonio/go-webserver v0.0.1
+)
+
+require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/slausonio/siogo v0.0.1 // indirect
+	golang.org/x/exp v0.0.0-20240103183307-be819d1f06fc // indirect
+	golang.org/x/sys v0.16.0 // indirect
+)
